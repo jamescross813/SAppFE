64e74c908b4e0881f943b8c17289944efa30cf41
jamescross813 <crossjames813@gmail.com>
2021-12-24 17:48:35 -0600

    basci homepage compenent

---
+++ src/components/HomepageComponent.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class Homepage extends Component{
+
+    render(){
+        return(
+            <div>
+                <HomepageNavBar />
+                    <h1>SApp</h1>
+                        <h4>About us</h4>
+                             <p>This app was created by a desperate man who was tied of losing posit notes with app ideas, to do lists and notes thought up in the middle of nowhere. A man who was sure there was a better way. A man who realised that until this app was up and running he'd just created the need for yet more posit notes....</p>
+            </div>
+        )
+    }
+}
+
+export default Homepage
