f46d32250d3ac9bcbb38b9f75f84f0c1264c8ea3
jamescross813 <crossjames813@gmail.com>
2021-12-24 17:40:15 -0600

    homepage component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom"
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Homepage from "./components/homepage/Homepage";
+
 
 function App() {
   return (
