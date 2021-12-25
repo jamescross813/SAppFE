7739bdd74b25977753ec821147ca26a1edaf3c1a
jamescross813 <crossjames813@gmail.com>
2021-12-24 19:35:18 -0600

    form added

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -3,9 +3,13 @@ import React, { Component } from 'react';
 class LoginComponent extends Component{
     render(){
         return(
-            <form>
-                
-            </form>
+            <form onSubmit={this.handleSubmit}>
+              <label>Username</label><br/>
+                <input type="text" name="username" value={this.state.username} onChange={this.handleChange} /><br/>
+              <label>Password</label><br/>
+                <input type="password" name="password" value={this.state.password} onChange={this.handleChange} /><br/>
+                <input type="submit" />
+          </form>
         )
     }
 }
