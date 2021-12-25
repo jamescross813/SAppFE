01ff83dc15212acb570925ebe553cca3fc22f5f0
jamescross813 <crossjames813@gmail.com>
2021-12-24 19:21:57 -0600

    navbar links

--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -4,7 +4,24 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="navbar">
-      
+      <NavLink 
+        to="/"
+        exact
+        >
+          Home
+        </NavLink>
+        <NavLink 
+        to="/projects"
+        exact
+        >
+          Projects
+        </NavLink>
+        <NavLink 
+        to="/to_dos"
+        exact
+        >
+          To Do
+        </NavLink>
     </div>
   );
 };
