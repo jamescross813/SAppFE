fe78950e3de2ec6d7ced4b89fcda05a33f1b2093
jamescross813 <crossjames813@gmail.com>
2021-12-24 19:36:16 -0600

    typo fix

--- src/components/HomepageComponent.js
+++ src/components/HomepageComponent.js
@@ -6,7 +6,7 @@ class Homepage extends Component{
     render(){
         return(
             <div>
-                <NavBar></NavBar>
+                <NavBar />
                 <h1>SApp</h1>
                     <h4>About us</h4>
                         <p>This app was created by a desperate man who was tied of losing posit notes with app ideas, to do lists and notes thought up in the middle of nowhere. A man who was sure there was a better way. A man who realised that until this app was up and running he'd just created the need for yet more posit notes....</p>
