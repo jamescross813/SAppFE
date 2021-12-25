e5eb7d13728c9469bf24fa8b37050c22952a945b
jamescross813 <crossjames813@gmail.com>
2021-12-24 19:38:34 -0600

    typo fix

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LoginComponent from '../components/LoginComponent'
 
-class LoginFormContainer extends Component {
+class LoginContainer extends Component {
 
     handleLogin = (userInfo)=>{
         let loginInfo = {
@@ -22,4 +22,4 @@ class LoginFormContainer extends Component {
       }
     
 }
-export default LoginFormContainer
+export default LoginContainer
