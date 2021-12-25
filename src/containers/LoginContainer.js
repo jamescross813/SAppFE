987d259b08297f7ee4c9909be905fd4063230d48
jamescross813 <crossjames813@gmail.com>
2021-12-24 19:28:36 -0600

    start handleLogin function

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class LoginFormContainer extends Component {
+
+    handleLogin = (userInfo)=>{
+
+    }
+
     render() {  
         return (
          <div>
