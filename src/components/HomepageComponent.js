65b5bb5e009daba35cbe845a10f62b024e2b7eec
jamescross813 <crossjames813@gmail.com>
2021-12-24 19:36:46 -0600

    added login form component

--- src/components/HomepageComponent.js
+++ src/components/HomepageComponent.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import NavBar from "./NavBarComponent";
+import LoginFormContainer from "../containers/LoginContainer";
 
 class Homepage extends Component{
 
@@ -7,6 +8,7 @@ class Homepage extends Component{
         return(
             <div>
                 <NavBar />
+                <LoginFormContainer />
                 <h1>SApp</h1>
                     <h4>About us</h4>
                         <p>This app was created by a desperate man who was tied of losing posit notes with app ideas, to do lists and notes thought up in the middle of nowhere. A man who was sure there was a better way. A man who realised that until this app was up and running he'd just created the need for yet more posit notes....</p>
