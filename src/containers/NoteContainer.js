932a5dca97810a18254319516fda06f009bbd4a6
jamescross813 <crossjames813@gmail.com>
2021-12-26 13:25:29 -0600

    basic model

---
+++ src/containers/NoteContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import NoteComponent from '../components/NoteComponent'
+
+class ProjectContainer extends Component {
+
+    render() {  
+        return (
+         <div>
+             <NoteComponent />
+         </div>
+        );
+      }
+    
+}
+export default NoteContainer
