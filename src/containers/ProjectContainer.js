bfe7947ff9c6cf922f6aad32d1d9f9f3031f914c
jamescross813 <crossjames813@gmail.com>
2021-12-26 13:22:16 -0600

    proejctcomponent render added

--- src/containers/ProjectContainer.js
+++ src/containers/ProjectContainer.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ProjectComponent from '../components/ProjectComponent'
 
 class ProjectContainer extends Component {
 
     render() {  
         return (
          <div>
-             
+             <ProjectComponent />
          </div>
         );
       }
