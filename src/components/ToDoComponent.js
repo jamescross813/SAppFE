0429a3313c52a76ff36ea5964e3ea400ef5213f8
jamescross813 <crossjames813@gmail.com>
2021-12-26 13:24:40 -0600

    basic model

---
+++ src/components/ToDoComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ToDoComponent extends Component {
+
+    render() {  
+        return (
+         <div>
+             <ProjectComponent />
+         </div>
+        );
+      }
+    
+}
+export default ToDoComponent
