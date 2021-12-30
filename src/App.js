835c99b294e1af1e4493700ba17f8120c72eba8e
jamescross813 <crossjames813@gmail.com>
2021-12-30 08:28:43 -0600

    login added to routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Homepage from "./components/HomepageComponent";
+import LoginContainer from "./containers/LoginContainer";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Router>
         <div className="App">
           <Route to exact path = "/" component={Homepage}/>
+          <Route to exact path = "login" component={LoginContainer} />
         </div>
       </Router>
       
