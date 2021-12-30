dc80979bb4262a8ffcd7a9810952fde52bdb7e39
jamescross813 <crossjames813@gmail.com>
2021-12-30 08:34:25 -0600

    basic user page conatiner class set up

---
+++ src/containers/UserPageContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import UserPageComponent from '../components/UserPageComponent'
+
+class UserPageContainer extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default UserPageContainer
