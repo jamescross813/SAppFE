f536cd34360690d3af4d0b4efe9f7631c4b797da
jamescross813 <crossjames813@gmail.com>
2021-12-30 08:31:46 -0600

    user page added to routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Homepage from "./components/HomepageComponent";
 import LoginContainer from "./containers/LoginContainer";
+import UserPageContainer from "./containers/UserPageContainer";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <div className="App">
           <Route to exact path = "/" component={Homepage}/>
           <Route to exact path = "login" component={LoginContainer} />
+          <Route to exact path = "/user" component={UserPageContainer} />
         </div>
       </Router>
       
