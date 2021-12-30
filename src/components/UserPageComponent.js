023ce5808c9700136f28e332af806756d6ab2c77
jamescross813 <crossjames813@gmail.com>
2021-12-30 08:36:41 -0600

    basic user page component set up

---
+++ src/components/UserPageComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class UserPageComponent extends Component {
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
