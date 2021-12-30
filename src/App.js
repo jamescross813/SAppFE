3cbe51877164ef0565d73faa8aa3f9b2dafcff68
jamescross813 <crossjames813@gmail.com>
2021-12-30 08:49:30 -0600

    added project contianer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 import Homepage from "./components/HomepageComponent";
 import LoginContainer from "./containers/LoginContainer";
 import UserPageContainer from "./containers/UserPageContainer";
-
+import ProjectContainer from "./containers/ProjectContainer"
 
 function App() {
   return (
@@ -12,7 +12,8 @@ function App() {
         <div className="App">
           <Route to exact path = "/" component={Homepage}/>
           <Route to exact path = "/login" component={LoginContainer} />
-          <Route to exact path = "/user" component={UserPageContainer} />
+          <Route to exact path = "/user/" component={UserPageContainer} />
+          <Route to exact path = "/user//projects" component={ProjectContainer}/>
         </div>
       </Router>
       
