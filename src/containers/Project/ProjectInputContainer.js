03d136d8964737cc68936b27f466bc9876074497
jamescross813 <crossjames813@gmail.com>
2022-01-02 17:43:03 -0600

    basic project input container model created

---
+++ src/containers/Project/ProjectInputContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ProjectInputComponent from "../../components/Project/ProjectInputComponent"
+
+class ProjectInputContainer extends Component{
+    render(){
+        return(
+            <div>
+                <ProjectInputComponent />
+            </div>
+        )
+    }
+}
+
+export default ProjectInputContainer
