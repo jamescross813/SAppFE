515b2017a2860ba07dfb275b4600c4da4bf76922
jamescross813 <crossjames813@gmail.com>
2022-01-02 17:34:50 -0600

    individual project page info route added

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
           <Route to exact path = "/login" component={LoginContainer} />
           <Route to exact path = "/user/" component={UserPageContainer} />
           <Route to exact path = "/user//projects" component={ProjectContainer}/>
+          <Route to exact path = "/projects/" component={ProjectContainer}/>
           <Route to exact path = "projects//todo" component={ToDoContainer} />
           <Route to exact path = "projects//note" component={NoteContainer} />
           <Route to exact path = "project//todo/add" component={ToDoInputContainer} />
