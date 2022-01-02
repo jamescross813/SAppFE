dc950e50c4820c86911de408a371d4b54922aba9
jamescross813 <crossjames813@gmail.com>
2022-01-02 17:47:11 -0600

    basic note input component

---
+++ src/components/Note/NoteInputComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NoteInputComponent extends Component{
+    render(){
+        return(
+            <div>
+                {/* render form to add new note or edit */}
+            </div>
+        )
+    }
+}
+
+export default NoteInputComponent
