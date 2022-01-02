5d080211c3448242f38c3db4f686958ef3f3a277
jamescross813 <crossjames813@gmail.com>
2022-01-02 17:49:25 -0600

    basic todo input container added

---
+++ src/containers/ToDo/ToDoInputContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ToDoInputComponent from "../../components/ToDo/ToDoInputComponent"
+
+class ToDoInputContainer extends Component{
+    render(){
+        return(
+            <div>
+                <ToDoInputComponent />
+            </div>
+        )
+    }
+}
+
+export default ToDoInputContainer
