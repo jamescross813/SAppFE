e3a5bc81a3f8dc118094b7c60340bb0a6f32f769
jamescross813 <crossjames813@gmail.com>
2022-01-02 17:43:56 -0600

    project input component basic added

---
+++ src/components/Project/ProjectInputComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ProjectInputComponent extends Component{
+    render(){
+        return(
+            <div>
+                {/* form to add and edit */}
+            </div>
+        )
+    }
+}
+
+export default ProjectInputComponent
