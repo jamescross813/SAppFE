22bf0260cb313e9a0ff262fcacf6c1ddaeda4ae7
jamescross813 <crossjames813@gmail.com>
2022-01-02 17:46:03 -0600

    basic note container model

---
+++ src/containers/Note/NoteInputContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NoteInputComponent from "../../components/Note/NoteInputComponent"
+
+class NoteInputContainer extends Component{
+    render(){
+        return(
+            <div>
+                <NoteInputComponent />
+            </div>
+        )
+    }
+}
+
+export default NoteInputContainer
