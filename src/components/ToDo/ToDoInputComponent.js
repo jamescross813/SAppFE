f08d341541228e160fa20295706b74142abb74a1
jamescross813 <crossjames813@gmail.com>
2022-01-02 17:50:35 -0600

    basic tdo component model

---
+++ src/components/ToDo/ToDoInputComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ToDoInputComponent extends Component{
+    render(){
+        return(
+            <div>
+               {/* render form for to do add and edit, include completed option? */}
+            </div>
+        )
+    }
+}
+
+export default ToDoInputComponent
