e5e2b026a9fced63ea4eb8de4fdf4ef6c3c0d6f7
jamescross813 <crossjames813@gmail.com>
2022-01-02 18:07:46 -0600

    bug track

---
+++ src/components/Project/ProjectsComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ProjectsComponent extends Component {
+
+    render() {  
+        return (
+         <div>
+            {/* render list of projects */}
+         </div>
+        );
+      }
+    
+}
+export default ProjectsComponent
