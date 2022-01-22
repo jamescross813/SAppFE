74fad89989314a6c92c439fabefaed2201f1d745
jamescross813 <crossjames813@gmail.com>
2022-01-21 22:48:25 -0600

    to do input form basic

--- src/components/ToDo/ToDoInputComponent.js
+++ src/components/ToDo/ToDoInputComponent.js
@@ -4,7 +4,11 @@ class ToDoInputComponent extends Component{
     render(){
         return(
             <div>
-               {/* render form for to do add and edit, include completed option? */}
+               <form onSubmit={this.handleSubmit}>
+                    <label>To Do Task:</label>
+                        <input type="text" name="to_do_task" value={this.state.to_do_task} onChange={this.handleChange}/><br/>
+                        
+                </form>
             </div>
         )
     }
